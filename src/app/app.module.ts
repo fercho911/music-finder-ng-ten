3ef5e6374e1aa1bf79339b41c8e9539121468363
Ferney Hoyos <kafer213@hotmail.com>
2021-09-01 10:51:56 -0500

    creation of base component music finder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { MusicFinderComponent } from './components/music-finder/music-finder.component'import { BrowserModule } from '@angular/platform-browser';
+import { MusicFinderComponent } from './components/music-finder/music-finder.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
