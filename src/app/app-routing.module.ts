ab94fa0d6033eb930d89841030ab4adbbadf3750
Ferney Hoyos <kafer213@hotmail.com>
2021-09-01 11:31:10 -0500

    setting up the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MusicFinderComponent } from './components/music-finder/music-finder.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',component: MusicFinderComponent},
+  { path: 'music-finder', component: MusicFinderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
